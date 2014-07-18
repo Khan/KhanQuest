111a5178f5814661714cd012ed90788a02d5739c
Charles Marsh <charlie@khanacademy.org>
2014-07-17 20:46:35 -0700

    Epic mixins and empty files

---
+++ src/game.jsx
@@ -0,0 +1,22 @@
+/** @jsx React.DOM */
+
+var React = require("react");
+
+var Game = React.createClass({
+    shouldComponentUpdate: function(nextProps, nextState) {
+        var stateChanged = !_.isEqual(this.state, nextState);
+        var propsChanged = !_.isEqual(this.props, nextProps);
+        return propsChanged || stateChanged;
+    },
+
+    propTypes: {
+        map: React.PropTypes.object,
+        user: React.PropTypes.object
+    },
+
+    render: function() {
+        return </div>;
+    }
+});
+
+module.exports = Game;
---
+++ src/map.jsx
@@ -0,0 +1,17 @@
+/** @jsx React.DOM */
+
+var React = require("react");
+var Changeable = require("mixins/changeable.jsx");
+
+var Map = React.createClass({
+    mixins: [Changeable],
+
+    propTypes: {
+    },
+
+    render: function() {
+        return null;
+    }
+});
+
+module.exports = Map;
