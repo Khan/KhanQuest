923faf0c6971d9915447488d9ee2347449df3260
Charles Marsh <charlie@khanacademy.org>
2014-07-18 11:32:37 -0700

    Basic spell casting

--- src/models/spell.js
+++ src/models/spell.js
@@ -1,9 +1,19 @@
 var allSpells = require("../data/spells.json");
 var _ = require("underscore");
 
+var SpellRepository = {
+    "measuring-lengths-1": function () {
+        console.log("Casting " + this.displayName);
+    },
+    "multiplying-fractions": function () {
+        console.log("Casting " + this.displayName);
+    }
+};
+
 function Spell (exerciseName) {
     this.exerciseName = exerciseName;
     _.extend(this, allSpells[exerciseName]);
+    this.cast = SpellRepository[exerciseName].bind(this);
 }
 
 module.exports = Spell;
