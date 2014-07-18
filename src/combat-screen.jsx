ed64e114dc17e5bf60747cd2c9188843f0582aed
Jack Toole <jack@khanacademy.org>
2014-07-17 23:11:30 -0700

    combat exercise

--- src/combat-screen.jsx
+++ src/combat-screen.jsx
@@ -1,48 +1,22 @@
 /** @jsx React.DOM */
 
 var React = require("react");
-var Perseus = require("perseus");
-
-var PERSEUS_ITEM = {
-    "question": {
-        "content": "Combat!",
-        "images": {},
-        "widgets": {}
-    },
-    "answerArea": {
-        "type": "multiple",
-        "options": {
-            "content": "",
-            "images": {},
-            "widgets": {}
-        },
-        "calculator": false
-    },
-    "itemDataVersion": {
-        "major": 0,
-        "minor": 1
-    },
-    "hints": []
-};
+var CombatExercise = require("./combat-exercise.jsx");
 
 var CombatScreen = React.createClass({
     propTypes: {
-        content: React.PropTypes.object.isRequired,
-        onAttack: React.PropTypes.func.isRequired
+        user: React.PropTypes.object,
+        enemy: React.PropTypes.object,
     },
 
     getDefaultProps: function() {
         return {
-            content: PERSEUS_ITEM,
-            onAttack: function() { }
         };
     },
 
     render: function() {
         return <div>
-            {Perseus.Renderer(this.props.content.question)}
-            {Perseus.AnswerAreaRenderer(this.props.content.answerArea)}
-            <button />
+            <CombatExercise />
         </div>;
     }
 });
