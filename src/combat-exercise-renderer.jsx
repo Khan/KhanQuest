e4a5c42fc9859dc2b04c8956795e5cda44de1e7d
Jack Toole <jack@khanacademy.org>
2014-07-17 23:46:14 -0700

    not working

---
+++ src/combat-exercise-renderer.jsx
@@ -0,0 +1,50 @@
+/** @jsx React.DOM */
+
+var React = require("react");
+var Perseus = require("perseus");
+
+var PERSEUS_ITEM = {
+    "question": {
+        "content": "Combat!",
+        "images": {},
+        "widgets": {}
+    },
+    "answerArea": {
+        "type": "multiple",
+        "options": {
+            "content": "",
+            "images": {},
+            "widgets": {}
+        },
+        "calculator": false
+    },
+    "itemDataVersion": {
+        "major": 0,
+        "minor": 1
+    },
+    "hints": []
+};
+
+var CombatExerciseRenderer = React.createClass({
+    propTypes: {
+        content: React.PropTypes.object.isRequired,
+        onAttack: React.PropTypes.func.isRequired
+    },
+
+    getDefaultProps: function() {
+        return {
+            content: PERSEUS_ITEM,
+            onAttack: function() { }
+        };
+    },
+
+    render: function() {
+        return <div>
+            {Perseus.Renderer(this.props.content.question)}
+            {Perseus.AnswerAreaRenderer(this.props.content.answerArea)}
+            <button />
+        </div>;
+    }
+});
+
+module.exports = CombatExerciseRenderer;
--- src/combat-exercise.jsx
+++ src/combat-exercise.jsx
@@ -2,48 +2,76 @@
 
 var React = require("react");
 var Perseus = require("perseus");
-
-var PERSEUS_ITEM = {
-    "question": {
-        "content": "Combat!",
-        "images": {},
-        "widgets": {}
-    },
-    "answerArea": {
-        "type": "multiple",
-        "options": {
-            "content": "",
-            "images": {},
-            "widgets": {}
-        },
-        "calculator": false
-    },
-    "itemDataVersion": {
-        "major": 0,
-        "minor": 1
-    },
-    "hints": []
-};
+var CombatExerciseRenderer = require("./combat-exercise-renderer.jsx");
 
 var CombatExercise = React.createClass({
     propTypes: {
-        content: React.PropTypes.object.isRequired,
+        spellName: React.PropTypes.string.isRequired,
         onAttack: React.PropTypes.func.isRequired
     },
 
     getDefaultProps: function() {
         return {
-            content: PERSEUS_ITEM,
+            spellName: "groups-of-tens",
             onAttack: function() { }
         };
     },
 
+    getInitialState: function() {
+        return {
+            content: null
+        };
+    },
+
+    componentWillMount: function() {
+        this._loadSpell();
+    },
+
+    componentWillReceiveProps: function(nextProps) {
+        if (nextProps.spellName !== this.props.spellName) {
+            this._loadSpell();
+        }
+    },
+
     render: function() {
-        return <div>
-            {Perseus.Renderer(this.props.content.question)}
-            {Perseus.AnswerAreaRenderer(this.props.content.answerArea)}
-            <button />
-        </div>;
+        if (this.state.content) {
+            return <div>
+                <CombatExerciseRenderer
+                    content={this.state.content}
+                    onAttack={this.props.onAttack} />;
+            </div>;
+        } else {
+            return <div>
+                Summoning spell...
+            </div>;
+        }
+    },
+
+    _loadSpell: function() {
+        $.ajax({
+            url: "http://www.khanacademy.org/api/v1/exercises/" +
+                this.props.spellName,
+            cache: false
+        }).then((exercise) => {
+            var items = exercise.all_assessment_items;
+            var index = _.random(0, items.length - 1);
+            var item = items[index];
+            return $.ajax({
+                url: "http://www.khanacademy.org/api/v1/assessment_items/" +
+                    item.id +
+                    "?version=" +
+                    item.sha,
+                cache: false
+            });
+        }).then((item) => {
+            // TODO(aria): Make this not break everything if we've received new
+            // props
+            this.setState({
+                content: item.item_data
+            });
+        }, (err) => {
+            console.error("ERROR LOADING ITEM: ", err);
+        });
     }
 });
 
