fc58b854727f7f85b166ed572459de87d5cefc03
Desmond Brand <dmnd@desmondbrand.com>
2014-07-18 13:42:53 -0700

    fix indentation

--- src/actions.jsx
+++ src/actions.jsx
@@ -7,25 +7,25 @@ var constants = {
 };
 
 var Actions = {
-     fetchMapData: function() {
-         AppDispatcher.handleViewAction({
-             actionType: constants.FETCH_MAP_DATA
-         });
-     },
+    fetchMapData: function() {
+        AppDispatcher.handleViewAction({
+            actionType: constants.FETCH_MAP_DATA
+        });
+    },
 
-     move: function(direction) {
-         AppDispatcher.handleViewAction({
-             actionType: constants.MOVE,
-             direction
-         });
-     },
+    move: function(direction) {
+        AppDispatcher.handleViewAction({
+            actionType: constants.MOVE,
+            direction
+        });
+    },
 
-     addSpell: function(exerciseName) {
+    addSpell: function(exerciseName) {
         AppDispatcher.handleViewAction({
             actionType: constants.ADD_SPELL,
             exerciseName: exerciseName
         });
-     }
+    }
 };
 
 module.exports = { constants, Actions };
