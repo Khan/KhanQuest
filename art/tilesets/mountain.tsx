864bce30460a8c8f8139f003bead78c73d6df7e8
Joel Burget <joel@khanacademy.org>
2014-07-17 23:18:30 -0700

    Add some assets we may use.

---
+++ art/tilesets/mountain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="mountain_landscape" tilewidth="32" tileheight="32">
+ <image source="mountain_landscape.png" width="512" height="512"/>
+</tileset>
---
+++ art/tilesets/wood.tsx
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="wood" tilewidth="32" tileheight="32">
+ <image source="wood_tileset.png" width="512" height="512"/>
+ <terraintypes>
+  <terrain name="grass" tile="0"/>
+  <terrain name="dirt" tile="19"/>
+  <terrain name="stone" tile="10"/>
+  <terrain name="long grass" tile="65"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,0"/>
+ <tile id="2" terrain="0,0,0,1"/>
+ <tile id="3" terrain="0,0,1,1"/>
+ <tile id="4" terrain="0,0,1,0"/>
+ <tile id="5" terrain="0,0,0,"/>
+ <tile id="6" terrain="0,0,,"/>
+ <tile id="7" terrain="0,0,,0"/>
+ <tile id="8" terrain="3,3,3,1"/>
+ <tile id="9" terrain="3,3,3,2"/>
+ <tile id="10" terrain="2,2,2,2"/>
+ <tile id="11" terrain="2,2,2,2"/>
+ <tile id="12" terrain="3,3,3,2"/>
+ <tile id="13" terrain=",,,3"/>
+ <tile id="14" terrain=",,3,3"/>
+ <tile id="15" terrain=",,3,"/>
+ <tile id="18" terrain="0,1,0,1"/>
+ <tile id="19" terrain="1,1,1,1"/>
+ <tile id="20" terrain="1,0,1,0"/>
+ <tile id="21" terrain="0,,0,"/>
+ <tile id="23" terrain=",0,,0"/>
+ <tile id="24" terrain="3,1,3,1"/>
+ <tile id="25" terrain="3,2,3,2"/>
+ <tile id="26" terrain="2,2,2,2"/>
+ <tile id="27" terrain="2,2,2,2"/>
+ <tile id="28" terrain="3,2,3,2"/>
+ <tile id="29" terrain=",3,,3"/>
+ <tile id="30" terrain="3,3,3,3"/>
+ <tile id="31" terrain="3,,3,"/>
+ <tile id="33" terrain="0,0,0,0"/>
+ <tile id="34" terrain="0,1,0,0"/>
+ <tile id="35" terrain="1,1,0,0"/>
+ <tile id="36" terrain="1,0,0,0"/>
+ <tile id="37" terrain="0,,0,0"/>
+ <tile id="38" terrain=",,0,0"/>
+ <tile id="39" terrain=",0,0,0"/>
+ <tile id="42" terrain="2,2,2,2"/>
+ <tile id="45" terrain=",3,,"/>
+ <tile id="46" terrain="3,3,,"/>
+ <tile id="47" terrain="3,,,"/>
+ <tile id="48" terrain="0,0,0,3"/>
+ <tile id="49" terrain="0,0,3,3"/>
+ <tile id="50" terrain="0,0,3,0"/>
+ <tile id="64" terrain="0,3,0,3"/>
+ <tile id="65" terrain="3,3,3,3"/>
+ <tile id="66" terrain="3,0,3,0"/>
+ <tile id="80" terrain="0,3,0,0"/>
+ <tile id="81" terrain="3,3,0,0"/>
+ <tile id="82" terrain="3,0,0,0"/>
+</tileset>
