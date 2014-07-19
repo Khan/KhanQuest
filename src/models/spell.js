9e53e14dc87416c6855017f4f95157fc35cf2f67
Charles Marsh <charlie@khanacademy.org>
2014-07-19 10:17:13 -0700

    refactored spells to use spell-store

--- src/models/spell.js
+++ src/models/spell.js
@@ -1,10 +1,10 @@
-var allSpells = require("../data/spells.json");
+var SpellStore = require("../spell-store.jsx");
 var _ = require("underscore");
 
 /* Grab data from spell.json datastore. */
 function Spell (exerciseName) {
     this.exerciseName = exerciseName;
-    _.extend(this, allSpells[exerciseName]);
+    _.extend(this, SpellStore.getById(exerciseName));
 }
 
 module.exports = Spell;
---
+++ src/spell-store.jsx
@@ -0,0 +1,54 @@
+/**
+ * A key-value store to hold the available spells and associated data.
+ * Loads a JSON file and provides id-based access. As with monster-store,
+ * this is not a flux datastore!
+ */
+
+var testJSON = JSON.stringify({
+    "measuring-lengths-1": {
+        displayName: "Fireball",
+        caption: "Interesting caption!",
+        description: "This is an epic spell, with tons of good powers, like damage.",
+        type: "fire",
+        power: 20,
+        cooldown: 3
+
+    },
+
+    "groups-of-tens": {
+        displayName: "Magic Missile",
+        caption: "(greensquadron)",
+        description: "Included to appease Jack.",
+        type: "arcane",
+        power: 20,
+        cooldown: 3
+    },
+
+    "decomposing-fractions": {
+        displayName: "Magic Missile",
+        caption: "(starmap)",
+        description: "What what what what what what what what what what what what what what what what what what what what.",
+        type: "arcane",
+        power: 20,
+        cooldown: 3
+    }
+});
+
+var _spellDict = {};
+
+var load = function(json) {
+    _spellDict = JSON.parse(json);
+};
+
+load(testJSON);
+
+var SpellStore = {
+    getById: function(spellId) {
+        return _spellDict[spellId];
+    },
+
+    debug: _spellDict
+};
+
+module.exports = SpellStore;
+
