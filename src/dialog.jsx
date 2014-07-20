333cc67f9842541d917711dee4a598141e4fbc01
Charles Marsh <charlie@khanacademy.org>
2014-07-20 10:37:09 -0700

    KUIButton for dialog

--- src/dialog.jsx
+++ src/dialog.jsx
@@ -2,6 +2,7 @@
 
 var React = require("react");
 var RP = React.PropTypes;
+var KUIButton = require("./components/button.jsx");
 
 var DialogData = require("./data/dialog.json");
 
@@ -28,13 +29,20 @@ var Dialog = React.createClass({
     render: function() {
         var line = this._getLine();
         var end = this.state.index === this._getScene().lines.length - 1;
-        var message = end ? "close" : "next";
+        var message = end ? "Close" : "Next";
         return <div className="dialog-view">
-            <div className="speaker">{line.speaker}</div>
-            <div className="line">{line.line}</div>
-            <button className="forward"
-                    type="button"
-                    onClick={this.onForward}>{message}</button>
+            <div style={{position: "relative"}}>
+                <div className="speaker">{line.speaker}</div>
+                <div className="line">{line.line}</div>
+                <div className="forward">
+                    <KUIButton type="submit"
+                        label={message}
+                        type="button"
+                        domainSlug="economics-finance-domain"
+                        width="140px"
+                        onClick={this.onForward} />
+                </div>
+            </div>
         </div>;
     },
 
